c69106d82d38f27ca07b2bb2641d2afad53a0aec
Dhanya MD <dhanya13md@gmail.com>
2025-05-05 23:24:30 +0530

    add

--- src/ai_companion/interfaces/whatsapp/webhook_endpoint.py
+++ src/ai_companion/interfaces/whatsapp/webhook_endpoint.py
@@ -3,4 +3,4 @@ from fastapi import FastAPI
 from ai_companion.interfaces.whatsapp.whatsapp_response import whatsapp_router
 
 app = FastAPI()
-app.include_router(whatsapp_router)
+app.include_router(whatsapp_router) 
